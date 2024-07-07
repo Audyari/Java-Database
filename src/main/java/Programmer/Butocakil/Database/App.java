0852ba4d7ed1a5d5094274cde846f1ca2fed3898
Audyari Wiyono <audy123ari@gmail.com>
2024-07-07 13:07:30 +0700

    Membuat Project

---
+++ src/main/java/Programmer/Butocakil/Database/App.java
@@ -0,0 +1,13 @@
+package Programmer.Butocakil.Database;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
